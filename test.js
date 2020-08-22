98dc7e69e84e38f9e82b6d63ee07d95b74de6241
Harshit Yaduka <harshit31yaduka@gmail.com>
2020-08-22 14:13:42 +0530

    forgot to end the respose method

--- test.js
+++ test.js
@@ -36,8 +36,10 @@ app.post('/sendmail',(req,res)=>{
   transporter.sendMail(mailOptions, function(error, info){
     if (error) {
       console.log(error);
+      res.end();
     } else {
       console.log('Email sent: ' + info.response);
+      res.end();
     }
   });
 
