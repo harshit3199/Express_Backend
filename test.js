2d29e63e0d0b22c9dc5054ff8a277b462b7b5ae3
Harshit Yaduka <harshit31yaduka@gmail.com>
2020-08-22 13:55:52 +0530

    Cannot send mail

---
+++ test.js
@@ -0,0 +1,42 @@
+const express= require('express');
+const path=require('path');
+const nodemailer=require('nodemailer');
+const app=express();
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname,'index.html'));
+});
+app.get('/about', (req,res)=>{
+  res.sendFile(path.join(__dirname,'about.html'));
+});
+app.get('/contact', (req,res)=>{
+  res.sendFile(path.join(__dirname, 'contact.html'));
+});
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'404.html'));
+});
+app.post('/sendmail',(req,res)=>{
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'harshit.yaduka@gmail.com',
+      pass: 'Harshit@3199'
+    }
+  });
+
+  var mailOptions = {
+    from: 'harshit.yaduka@gmail.com',
+    to: req.body.email,
+    subject: 'Sending Email using Node.js'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+})
+const PORT= 5000;
+app.listen(PORT,()=> console.log(`Server Started in port ${PORT}`));
